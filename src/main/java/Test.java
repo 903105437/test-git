67791b253bb6267148b1b2740a4aea5ec3807cae
xiaowang <wds@qq.com>
2018-04-28 09:16:46 +0800

    第四次提交

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("我擦勒");
         System.out.println("呵呵  另一个窗口");
+        System.out.println("主窗口的提交");
     }
 }
